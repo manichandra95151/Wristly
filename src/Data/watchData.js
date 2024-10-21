9eb4b2802f58f7a6a8e09c825c3112c883b0426b
manichandra95151 <manichandra9151@gmail.com>
2024-10-21 19:35:34 +0530

    Updated Watches

---
+++ src/Data/watchData.js
@@ -0,0 +1,169 @@
+export const watches=[
+    {
+        id:1,
+        name:"Greca Logo Watch",
+        brand:"Versace",
+        price:2780.00,
+        imageUrl:"https://images.pexels.com/photos/3766111/pexels-photo-3766111.jpeg?auto=compress&cs=tinysrgb&w=600"
+    },
+    {
+        id:2,
+        name:"Fitbit Sense 2 Smart Watch",
+        brand:"Fitbit",
+        price:2780.00,
+imageUrl:"https://images.pexels.com/photos/9561296/pexels-photo-9561296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"    },
+    {
+        id:3,
+        name:"Morris & Co 30mm Forest Hare Watch",
+        brand:"August Berg",
+        price:329.95,
+imageUrl:"https://images.pexels.com/photos/16739807/pexels-photo-16739807/free-photo-of-close-up-of-a-black-mans-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"    },
+    {
+        id:4,
+        name:"Townsman Dark Brown Automatic Mechanical Watch",
+        brand:"Fossil",
+        price:479.00,
+imageUrl:"https://images.pexels.com/photos/9423289/pexels-photo-9423289.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"    },
+    {
+        id:5,
+        name:"MS9 Red Analogue Watch DZ1930",
+        brand:"Diesel",
+        price:249.00,
+imageUrl:"https://images.pexels.com/photos/4863073/pexels-photo-4863073.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"    },
+    {
+        id:6,
+        name:"Fossil Townsman Black Watch ME3210",
+        brand:"Fossil",
+        price:479.00,
+imageUrl:"https://images.pexels.com/photos/5447382/pexels-photo-5447382.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"    },
+    {
+        id:7,
+        name:"Two Tone Analogue Watch AR80035",
+        brand:"Emporio Armani",
+        price:889.00,
+imageUrl:"https://images.pexels.com/photos/12582201/pexels-photo-12582201.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"    },
+    {
+        id:8,
+        name:"Purple Analogue Watch AR11269",
+        brand:"Emporio Armani",
+        price:599.00,
+imageUrl:"https://images.pexels.com/photos/3083461/pexels-photo-3083461.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"    },
+    {
+        id:9,
+        name:"Mega Chief Multicolour Analogue-Digital Watch",
+        brand:"Diesel",
+        price:499.00,
+imageUrl:"https://images.pexels.com/photos/8839604/pexels-photo-8839604.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"    },
+    {
+        id:10,
+        name:"Scarlette Rose Gold-Tone Analogue Watch ES4318",
+        brand:"Fossil",
+        price:249.00,
+imageUrl:"https://images.pexels.com/photos/15649355/pexels-photo-15649355/free-photo-of-close-up-of-modern-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"    },
+    {
+        id:11,
+        name:"Icon Active - VEZ7 44mm",
+        brand:"Versace",
+        price:1850.00,
+        imageUrl:"https://images.pexels.com/photos/16739805/pexels-photo-16739805/free-photo-of-close-up-of-a-black-mans-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"    },
+
+    {
+        id:12,
+        name:"Silver Women's Analogue Watch",
+        brand:"Emporio Armani",
+        price:579.00,
+        imageUrl:"https://images.pexels.com/photos/18722865/pexels-photo-18722865/free-photo-of-close-up-of-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"    },
+    {
+        id:13,
+        name:"Jacqueline Women's Analogue Watch",
+        brand:"Fossil",
+        price:299.00,
+        imageUrl:"https://images.pexels.com/photos/8839604/pexels-photo-8839604.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+    },
+    {
+        id:14,
+        name:"Mega Chief Men's Chronograph Watch",
+        brand:"Diesel",
+        price:449.00,
+      imageUrl:"https://images.pexels.com/photos/18203368/pexels-photo-18203368/free-photo-of-close-up-of-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+    },
+    {
+        id:15,
+        name:"Rose Gold-Tone Analogue Watch AR11266",
+        brand:"Emporio Armani",
+        price:779.00,
+        imageUrl:"https://images.pexels.com/photos/7617108/pexels-photo-7617108.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+    },
+    {
+        id:16,
+        name:"The Upper East Side",
+        brand:"Rosefield",
+        price:189.95,
+imageUrl:"https://images.pexels.com/photos/7844657/pexels-photo-7844657.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+    },
+
+    {
+        id:17,
+        name:"Green Men's Digital Watch",
+        brand:"Armani Exchange",
+        price:199.00,
+        imageUrl:"https://images.pexels.com/photos/9561403/pexels-photo-9561403.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+    },{
+        id:18,
+        name:"The Octagon Charm",
+        brand:"Rosefield",
+        price:249.95,
+       imageUrl:"https://images.pexels.com/photos/8892886/pexels-photo-8892886.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+    },
+    {
+        id:19,
+        price:349.00,
+        brand:"Fossil",
+        name:"Fossil Fenmore Midsize Navy Blue Watch BQ2403",
+        imageUrl:"https://images.pexels.com/photos/12697589/pexels-photo-12697589.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+    },
+    {
+        id:20,
+        name:"Contour White Analogue Watch",
+        brand:"Puma",
+        price:109.00,
+        imageUrl:"https://images.pexels.com/photos/4521458/pexels-photo-4521458.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"    },
+    {
+        id:21,
+        name:"Gold-Tone Women's Analogue Watch",
+        brand:"Emporio Armani",
+        price:499.00,
+        imageUrl:"https://images.pexels.com/photos/27132434/pexels-photo-27132434/free-photo-of-close-up-of-a-watch.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"    },
+
+    {
+        id:22,
+        name:"Fossil Carlie Rose Gold Watch ES5158",
+        brand:"Fossil",
+        price:289.00,
+        imageUrl:"https://images.pexels.com/photos/9423283/pexels-photo-9423283.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+    },
+    {
+        id:23,
+        name:"Contour White Analogue Watch P1018",
+        brand:"Puma",
+        price:539.00,
+        imageUrl:"https://images.pexels.com/photos/9203637/pexels-photo-9203637.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"    },
+
+    {
+        id:24,
+        name:"Fossil Neutra Minimalist Brown Watch FS5903",
+        brand:"Fossil",
+        price:299.00,
+        imageUrl:"https://images.pexels.com/photos/27627757/pexels-photo-27627757/free-photo-of-a-silver-watch-on-an-orange-background.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"    },
+    
+    {
+        id:25,
+        name:"Multi-Tone Analogue Watch",
+        brand:"Emporio Armani",
+        price:779.00,
+        imageUrl:"https://images.pexels.com/photos/17572540/pexels-photo-17572540/free-photo-of-close-up-of-a-luxurious-wristwatch.png?auto=compress&cs=tinysrgb&w=600&lazy=load" 
+    },
+
+
+    
+]
